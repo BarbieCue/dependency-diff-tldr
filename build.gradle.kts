428ed5594f933c2651fe5be0da9af93c09ea826e
René Knop <rene@kn0p.family>
2024-11-13 20:03:32 +0100

    Refactor build

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+  id("org.jetbrains.kotlin.jvm") version "2.0.21"
+  id("io.ktor.plugin") version "3.0.1"
+  id("application")
+  `jvm-test-suite`
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(libs.kotlinx.cli)
+}
+
+val mainClassName = "com.careem.gradle.dependencies.DependencyTreeTldr"
+
+application {
+  mainClass.set(mainClassName)
+}
+
+ktor {
+  fatJar {
+    archiveFileName.set("gradle-dependency-diff.jar")
+  }
+}
+
+testing {
+  suites {
+    val test by getting(JvmTestSuite::class) {
+      useJUnitJupiter()
+      dependencies {
+        implementation(libs.kotest.runner.junit5)
+        implementation(libs.kotest.assertions.core)
+      }
+    }
+  }
+}
