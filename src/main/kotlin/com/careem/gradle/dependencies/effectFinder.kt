8ef9c8f3f748f2be87ba940b2b1aa663c8322e30
René Knop <rene@kn0p.family>
2024-11-13 20:11:43 +0100

    Move extension functions from util.kt to effectFinder.kt

--- src/main/kotlin/com/careem/gradle/dependencies/effectFinder.kt
+++ src/main/kotlin/com/careem/gradle/dependencies/effectFinder.kt
@@ -16,11 +16,6 @@
 
 package com.careem.gradle.dependencies
 
-import com.careem.gradle.dependencies.common.filterMatches
-import com.careem.gradle.dependencies.common.parser.Dependency
-import com.careem.gradle.dependencies.common.parser.parseDependencyTree
-import com.careem.gradle.dependencies.common.writeTree
-
 fun upgradeEffects(old: String, new: String, collapseKeys: List<String>): String {
     val (_, _, upgrades) = dependencyDifferences(old, new)
     return if (upgrades.isNotEmpty()) {
@@ -78,4 +73,27 @@ fun upgradeEffects(old: String, new: String, collapseKeys: List<String>): String
     }
 }
 
-internal fun Dependency.matches(target: String) = artifact == target
+private fun Dependency.matches(target: String) = artifact == target
+
+private fun Dependency.filterMatches(lambda: (Dependency) -> Boolean): Dependency? {
+    return if (lambda(this)) {
+        this.copy(children = emptyList())
+    } else {
+        val children = this.children.mapNotNull { it.filterMatches(lambda) }
+        if (children.isNotEmpty()) {
+            this.copy(children = children)
+        } else {
+            null
+        }
+    }
+}
+
+private fun StringBuilder.writeTree(dependency: Dependency, indent: Int) {
+    appendLine()
+    append(" ".repeat(indent * 4))
+    append(dependency.artifact)
+    append(" (")
+    append(dependency.resolvedVersion)
+    append(")")
+    dependency.children.forEach { writeTree(it, indent + 1) }
+}
